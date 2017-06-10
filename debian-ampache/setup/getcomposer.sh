8bdd922d9a7d69702bcd425fb479683c4b269540
John Goerzen <jgoerzen@complete.org>
2017-06-10 13:16:37 -0500

    Initial build complete

--- debian-ampache/setup/getcomposer.sh
+++ debian-ampache/setup/getcomposer.sh
@@ -1,4 +1,4 @@
-i#!/bin/sh
+#!/bin/sh
 
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
--- debian-ampache/setup/setup.sh
+++ debian-ampache/setup/setup.sh
@@ -5,12 +5,14 @@ set -x
 
 cd /tmp/setup
 /tmp/setup/getcomposer.sh
+mv -vi composer.phar /usr/local/bin/composer
 
 wget https://github.com/ampache/ampache/archive/3.8.2.tar.gz
 sha256sum -c < sums
 
 cd /var/www
 tar -zxvf /tmp/setup/3.8.2.tar.gz
+mv ampache-3.8.2 ampache
 
 exec rm -rf /tmp/setup
 
