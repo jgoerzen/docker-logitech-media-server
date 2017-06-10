b7d84f115b60e64a2c049206715b44ee84a55877
John Goerzen <jgoerzen@complete.org>
2017-06-10 15:03:55 -0500

    Switch to Github for latest

--- ampache/setup/setup.sh
+++ ampache/setup/setup.sh
@@ -7,12 +7,10 @@ cd /tmp/setup
 /tmp/setup/getcomposer.sh
 mv -vi composer.phar /usr/local/bin/composer
 
-wget https://github.com/ampache/ampache/archive/3.8.2.tar.gz
-sha256sum -c < sums
-
 cd /var/www/html
-tar -zxvf /tmp/setup/3.8.2.tar.gz
-mv ampache-3.8.2 ampache
+git clone https://github.com/ampache/ampache.git
+cd ampache
+git checkout master
 
 exec rm -rf /tmp/setup
 
